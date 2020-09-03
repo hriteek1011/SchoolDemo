b5f360b82eff98b197773ee6471878246c03105c
hriteek1011 <68454427+hriteek1011@users.noreply.github.com>
2020-09-03 09:41:10 +0530

    Add files via upload

---
+++ project6.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
+$(document).ready(function () {
+  $(".single-image").click(function(){
+    var t = $(this).attr("src");
+    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $("#myModal").modal();
+  });
+});
